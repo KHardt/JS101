6233abcc6f313730a3d2804107df1a0adbc80ecf
Klaus Hardt <klausheina@gmail.com>
2018-07-13 14:51:34 -0500

    added stuff

---
+++ LightningExercises/Exercise3/main.js
@@ -0,0 +1,15 @@
+/*1. Create a function that logs the result of adding two numbers that were passed in as arguments.
+2. Create another function that logs the result of subtracting two numbers that were passed in as arguments.
+3. Invoke each function a couple times with different argument values. */
+
+function adding (num1, num2) {
+    console.log(num1 + num2)
+    
+}
+adding(3, 4)
+
+function subtracting (num1, num2) {
+    console.log(num1 - num2)
+}
+subtracting(3, 4)
+
