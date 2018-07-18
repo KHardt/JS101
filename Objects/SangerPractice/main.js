4492c56488e9c6606f4294e554b0e896c7cf9d2b
Klaus Hardt <klausheina@gmail.com>
2018-07-18 10:35:00 -0500

    started practice

---
+++ Objects/SangerPractice/main.js
@@ -0,0 +1,43 @@
+const employee = {
+    firstName: "Michael",
+    lastName: "Tambornino",
+    role: "Sales Rep",
+    accountNumber: "1-8349058340"
+}
+
+const antiqueSecretary = {
+    name: "Beidemieher Secretary",
+    location: "Mom's Basement",
+    description: "Belonged to Grandmother. Antique. Value unknown."
+  }
+
+  const districtInfo = {
+        name: "5th Congressional District",
+        location: "Nashville, TN",
+        Time: "2003-present"
+  }
+
+  const platFormStatements = {
+      Taxes: "lower",
+      Jobs: "reduce outsourcing",
+      Infrastructure: "fix highways",
+      HealthCare: "pro Obama-care",
+      Crime_Enforcement: "increase funding"
+  }
+
+  const donationForm = {
+      url: "https://en.wikipedia.org/wiki/Jim_Cooper",
+  }
+
+  const eventCalender = {
+      July: "MAD speaking engagement",
+      August: "Party meeting",
+      Sept: "Titan's Dinner event"
+  }
+
+  const volunteerInfo = [
+    volList: {
+
+    }
+
+]
