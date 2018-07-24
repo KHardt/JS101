0d9034e8769c1b2ef05aa12492df6c133d3912ff
Klaus Hardt <klausheina@gmail.com>
2018-07-23 22:47:30 -0500

    stuff that works..

--- Objects/SangerPractice/main.js
+++ Objects/SangerPractice/main.js
@@ -59,7 +59,42 @@ const voterRegistration = {
     url: "",
 }
 
-sangerDB.push(districtInfo, platFormStatements);
+sangerDB.push(districtInfo, platFormStatements, donationForm, eventCalender, volunteerList, personalBio, imageGallery, missionStatement, voterRegistration);
+//console.log(sangerDB)
+//console.log(sangerDB[0])
+
+function changeDistrictInfo (name, location, Time) {
+    sangerDB[0].name = name;
+    sangerDB[0].location = location;
+    sangerDB[0].Time = Time;
+}
+
+changeDistrictInfo("6th", "Cookeville", "Present") //works!
+//console.log(sangerDB)
+
+function changePlatform (taxes, jobs, infra, healthcare, crime) {
+    sangerDB[1].Taxes = Taxes;
+    sangerDB[1].Jobs = Jobs;
+    sangerDB[1].Infrastructure = Infrastructure;
+    sangerDB[1].HealthCare = HealthCare;
+    sangerDB[1].Crime_Enforcement = Crime_Enforcement
+}
+
+function changeDonationForm (currentURL) {
+    sangerDB[2].url = url;
+}
+
+//the below works but order is scrambled in consolelog
+function changeEventCal (Month, newEvent) {
+    return sangerDB[3][Month] = newEvent;
+}
+changeEventCal("October", "nothing")
+
 console.log(sangerDB)
-console.log(sangerDB[0])
+
+//the below actually f-in worked: ...
+let findDiv = document.querySelector("#fif")
+let addStuff = document.createElement("h3")
+addStuff.textContent = sangerDB[0].repName;
+findDiv.appendChild(addStuff)
 
