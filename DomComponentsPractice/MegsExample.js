b24f57de6daa168a271434664c2eb9b86d2e9fed
Klaus Hardt <klausheina@gmail.com>
2018-07-27 15:53:00 -0500

    practice p1 and 2

---
+++ DomComponentsPractice/MegsExample.js
@@ -0,0 +1,24 @@
+Review
+addCustomer: {
+    value: function(customer) {
+        this.customers.push(customer)
+    }
+
+}
+coffeecompany.addCustomer()
+
+let customers = 
+//function for object create and using object create
+
+//cannot foreach over objects- instead 
+debugger- can place anywhere
+
+
+fragment:
+create fragment
+create element
+title.textcontent = 
+fragment.appendChild(title)
+output.appendChild(fragment)
+r
+
--- DomComponentsPractice/main.js
+++ DomComponentsPractice/main.js
@@ -5,38 +5,57 @@ Create five (5) section elements, each with a class of message, and with textCon
 Using appendChild(), attach each message as a child to the messages element. */
 
 //step1:
-const article = document.createElement('article')
+const article = document.createElement("article")
 article.id = "messages"
+//article.appendChild()-- //need to append it on the dom to work
 console.log(article)
 
 //step2:
 const findArticle = document.querySelector("article")
+console.log(findArticle)
 
 //step3:
 const createSection1 = document.createElement("section")
-createSection1.className = "message";
+//createSection1.className = "message";
 createSection1.textContent = "SUP Aunty"
 //console.log(createSection1)
 
 const createSection2 = document.createElement("section")
-createSection2.className = "message";
+//createSection2.className = "message";
 createSection2.textContent = "o it's annoying nephew"
 
 const createSection3 = document.createElement("section")
-createSection3.className = "message";
+//createSection3.className = "message";
 createSection3.textContent = "LOL"
 
 const createSection4 = document.createElement("section")
-createSection4.className = "message";
+//createSection4.className = "message";
 createSection4.textContent = "your dad is not your real father"
 
 const createSection5 = document.createElement("section")
-createSection5.className = "message";
+//createSection5.className = "message";
 createSection5.textContent = "that's not funny aunty"
 
+/*
 article.appendChild(createSection1)
 article.appendChild(createSection2)
 article.appendChild(createSection3)
 article.appendChild(createSection4)
 article.appendChild(createSection5)
+*/
 
+
+const fragment = document.createDocumentFragment()
+
+fragment.appendChild(createSection1)
+fragment.appendChild(createSection2)
+fragment.appendChild(createSection3)
+fragment.appendChild(createSection4)
+fragment.appendChild(createSection5)
+
+console.log(fragment)
+//fragment part is working with body tag only...
+document.getElementById("output").appendChild(fragment)
+
+
+//i need to use body. document.body.....
