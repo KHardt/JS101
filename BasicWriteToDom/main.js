695a754ffa9cee500b0d5f163c2c1dbda395747f
Klaus Hardt <klausheina@gmail.com>
2018-07-27 07:49:24 -0500

    one of two done

--- BasicWriteToDom/main.js
+++ BasicWriteToDom/main.js
@@ -1,13 +1,28 @@
-let myCost = 3/1.5;
-console.log(myCost);
+//let myCost = 3/1.5;
+//console.log(myCost);
 
-let phrase = "The quick brown fox";
+/*let phrase = "The quick brown fox";
 
 let newStuff = `<h2>My Favorite Poems of Ice Cream</h2>
                 <p>${phrase}</p>
                 <h3>By Brenda Long</h3>`;
 
-console.log(newStuff)
+console.log(newStuff) */
 
-let addToDom = document.getElementById("demo");
-addToDom.innerHTML = newStuff;
+const addToDom = document.querySelector(".article_header");
+addToDom.textContent = "Welcome the Klausinhagen blog";
+
+const allHeaders = document.querySelectorAll(".article_header", "div")
+allHeaders.className = "article_header.important" 
+
+//test to see what is getting hit:
+/*var x = document.querySelectorAll(".article_header");
+    x[0].style.backgroundColor = "red";*/
+
+var test1 = document.querySelectorAll(".article_header").length
+console.log(test1)
+
+const test2 = document.querySelector("div")//.querySelectorAll(".article_header")
+test2.className = "article_header.important" 
+
+//notes to ask- can a class name be with spaces like article_ header importnant?
