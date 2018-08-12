5f94eb2f497e5389cd5e1997eef087697c9f5de7
Klaus Hardt <klausheina@gmail.com>
2018-08-12 11:18:51 -0500

    finished this practice sorta.

--- Objects/FinAdvisorExercise/main.js
+++ Objects/FinAdvisorExercise/main.js
@@ -15,44 +15,91 @@ const finAdvisor = Object.create(null, {
 
     name: {
         value: "Deborah Long",
-        enumerable: true
+        writable: true //tested security feature by setting to false
     },
 
     portfolio: {
-        value: ["Amazon", "Exxon", "Google"],
-        enumerable: false
+        value: [
+            {
+                stock: "Amz",
+                price: 34,
+                quantity: 65
+            }
+        ],
+        enumerable: true
     },
 
     worth: {
-        value: function () {
-            const netWorth = "";
-            for (let i =0; i < portfolio.value.length; i++)
-            netWorth = portfolio.value.length[i];
+            value: function() {
+            let netWorth = 0;
+            for (let i =0; i < this.portfolio.length; i++) {
+            let stockWorth = this.portfolio[i].quantity * this.portfolio[i].price;
+            netWorth += stockWorth
         }
+        return netWorth
+    },
+        enumerable: true,
+        writable: true
     },
 
     purchase: {
         enumerable: false,
-        purchaseStuff: function (stock, price, quantity) {
-            finAdvisor.portfolio.value ++;
+        value: function (stock, price, quantity) {
+            let purchaseStock = {
+                stock: stock,
+                price: price,
+                quantity: quantity,
+               
+        }
+        finAdvisor.portfolio.push(purchaseStock)
+    }
+},
+
+
+
+//don't really understand Meg's logic on the sell function. Since OR statement based on price or quantity could pick the first match regardless of stock name. Unless Im missing something which i prob am. Went with Will's recommendation which is to target the stock name- then make the calculations. Array method I chose is irrelevant
+    sell: {
+        value: function (stock, price, quantity) {
+            let portfolioArray = finAdvisor.portfolio;
+            portfolioArray.forEach(function (element) {
+                if (stock === element.stock) {
+                    element.price -= price;
+                    element.quantity -= quantity;
+                }
+            })
         }
     },
-
+    /*
     sell: {
-        enumerable: false,
-        sellStuff: function (stock, price, quantity) {
-        finAdvisor.portfolio.value ++;
-        }
-    }
-
-
-
+        value: function (stock, price, quantity) {
+             let removeStock =  {
+                stock: stock,
+                quantity: quantity,
+                price: price
+                
+            }
+            }
+            finAdvisor.portfolio.slice(removeStock)
+        
+        
+        }*/
     
-}  
-)
+     
+    });
 
-console.log(finAdvisor.company)
 
-test = document.createElement("div1")
-test.appendChild()
+//console.log(finAdvisor)
+finAdvisor.name ="Brenda Long";//apparently Deborah is incorrect!
+//console.log(finAdvisor.name)
+
+finAdvisor.purchase("TMZ", 25, 400); //purchase updates portforlio
+console.log(finAdvisor.portfolio)
+
+finAdvisor.sell("TMZ", 25, 400)
+console.log(finAdvisor.worth())
+
+/*
+const addStuff = document.querySelector("#advisorInfo")
+const testPage = document.createElement("article");
+addStuff.appendChild(testPage) */
 
